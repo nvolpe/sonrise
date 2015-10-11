c6120aaf86d40d98f737dbecd97ba5889a00c2d4
nvolpe <nvolpe@dtsagile.com>
2015-10-11 11:42:43 -0600

    ERROR PAGE!

---
+++ sonrise/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sonrise.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Error Page";
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            ViewBag.Title = "Not Found Page";
+            return View();
+        }
+    }
+}
